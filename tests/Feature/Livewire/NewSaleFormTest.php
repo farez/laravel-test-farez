d90c0e7ae0f986a5a9a2a0a41d12ca547ce99900
Farez Rahman <farez@redkeydigital.com>
2022-06-14 12:38:48 +0100

    Added test: sale details are visible on sales page after creation.

--- tests/Feature/Livewire/NewSaleFormTest.php
+++ tests/Feature/Livewire/NewSaleFormTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Livewire;
 
 use App\Http\Livewire\NewSaleForm;
+use App\Http\Livewire\PreviousSales;
 use App\Models\Sale;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,6 +15,10 @@ use Tests\TestCase;
 class NewSaleFormTest extends TestCase
 {
     use RefreshDatabase;
+
+    /**
+     * TESTS FOR NEW SALE FORM COMPONENT
+     */
 
     /** @test */
     public function the_component_can_render()
@@ -41,13 +46,6 @@ class NewSaleFormTest extends TestCase
     public function sales_page_not_visible_publicly()
     {
         $this->get(route('coffee.sales'))->assertSee('login');
-    }
-
-    /** @test */
-    public function sales_page_contains_previous_sales_component()
-    {
-        $this->actingAs(User::factory()->create());
-        $this->get(route('coffee.sales'))->assertSeeLivewire('previous-sales');
     }
 
     /** @test */
@@ -143,5 +141,44 @@ class NewSaleFormTest extends TestCase
             ->where('shipping_cost', '=', $shippingCost)
             ->where('selling_price', '=', $sellingPrice)
             ->exists());
+    }
+
+    /**
+     * TESTS FOR PREVIOUS SALES COMPONENT
+     */
+
+    /** @test */
+    public function sales_page_contains_previous_sales_component()
+    {
+        $this->actingAs(User::factory()->create());
+        $this->get(route('coffee.sales'))->assertSeeLivewire('previous-sales');
+    }
+
+    /** @test */
+    public function sale_details_are_visible_on_sales_page()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $product = 'Gold coffee';
+        $quantity = 3;
+        $unitCost = 5.50;
+        $cost = $quantity * $unitCost;
+        $profitMargin = config('coffee.profit_margin');
+        $shippingCost = config('coffee.shipping_cost');
+        $sellingPrice = ($cost / (1 - $profitMargin)) + $shippingCost;
+
+        Livewire::test(NewSaleForm::class, [
+            'product' => $product,
+            'quantity' => $quantity,
+            'unitCost' => $unitCost,
+            'profit_margin' => $profitMargin,
+            'shipping_cost' => $shippingCost,
+            'selling_price' => $sellingPrice,
+        ])->call('createSale');
+
+        Livewire::test(PreviousSales::class)
+            ->assertSee($quantity)
+            ->assertSee($unitCost)
+            ->assertSee($sellingPrice);
     }
 }
