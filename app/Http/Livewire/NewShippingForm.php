85b23db630b0bf6fca120f9eccf2f434b71c77c9
Farez Rahman <farez@redkeydigital.com>
2022-06-14 17:00:24 +0100

    Made the shipping charges form functional, including validation.

--- app/Http/Livewire/NewShippingForm.php
+++ app/Http/Livewire/NewShippingForm.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\ShippingCharge;
 use Livewire\Component;
 
 class NewShippingForm extends Component
 {
+    public $shippingCost;
+
+    protected $rules = [
+        'shippingCost' => 'required|numeric|gt:0',
+    ];
+
     public function render()
     {
         return view('livewire.new-shipping-form');
+    }
+
+    public function createShippingCharge()
+    {
+        $this->validate();
+
+        ShippingCharge::create([
+            'cost' => $this->shippingCost,
+            'number_of_sales' => 0,
+        ]);
+
+        $this->shippingCost = '';
     }
 }
--- resources/views/livewire/new-shipping-form.blade.php
+++ resources/views/livewire/new-shipping-form.blade.php
@@ -1,11 +1,11 @@
 <div class="sm:w-3/4">
     <form wire:submit.prevent="createShippingCharge" class="flex flex-col sm:w-1/4">
         <div>
-            <label for="charge" class="block text-sm font-medium text-gray-700"> {{ __('New cost of shipment') }} </label>
+            <label for="shippingCost" class="block text-sm font-medium text-gray-700"> {{ __('New cost of shipment') }} </label>
             <div class="mt-1">
-                <input wire:model="charge" type="text" name="charge" id="charge" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                <input wire:model="shippingCost" type="text" name="shippingCost" id="shippingCost" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
             </div>
-            @error('charge') <span class="text-red-700">{{ $message }}</span> @enderror
+            @error('shippingCost') <span class="text-red-700">{{ $message }}</span> @enderror
         </div>
         <div class="mt-2">
             <button wire:loading.remove wire:target="createShippingCharge" type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{{ __('Set new price') }}</button>
