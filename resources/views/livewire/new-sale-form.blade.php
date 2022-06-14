371b1bb79e7c311fabe30b56237dffea9017e780
Farez Rahman <farez@redkeydigital.com>
2022-06-14 18:18:25 +0100

    Tidied up the layout of the sales form.

--- resources/views/livewire/new-sale-form.blade.php
+++ resources/views/livewire/new-sale-form.blade.php
@@ -11,21 +11,18 @@
                         @endforeach
                     </select>
                 </div>
-                @error('product') <span class="text-red-700">{{ $message }}</span> @enderror
             </div>
             <div>
                 <label for="quantity" class="block text-sm font-medium text-gray-700"> {{ __('Quantity') }} </label>
                 <div class="mt-1">
                     <input wire:model="quantity" type="text" name="quantity" id="quantity" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
                 </div>
-                @error('quantity') <span class="text-red-700">{{ $message }}</span> @enderror
             </div>
             <div>
                 <label for="unitCost" class="block text-sm font-medium text-gray-700"> {{ __('Unit Cost £') }} </label>
                 <div class="mt-1">
                     <input wire:model="unitCost" type="text" name="unitCost" id="unitCost" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
                 </div>
-                @error('unitCost') <span class="text-red-700">{{ $message }}</span> @enderror
             </div>
             <div>
                 <label for="sellingPrice" class="block text-sm font-medium text-gray-700"> {{ __('Selling Price') }} </label>
@@ -38,6 +35,13 @@
                 <button wire:loading.remove wire:target="createSale" type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{{ __('Record sale') }}</button>
                 <button wire:loading wire:target="createSale" disabled class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-600">{{ __('Saving...') }}</button>
             </div>
+        </div>
+        <div class="grid grid-cols-5 gap-4 align-baseline">
+            <div>@error('product') <span class="text-red-700">{{ $message }}</span> @enderror</div>
+            <div>@error('quantity') <span class="text-red-700">{{ $message }}</span> @enderror</div>
+            <div>@error('unitCost') <span class="text-red-700">{{ $message }}</span> @enderror</div>
+            <div></div>
+            <div></div>
         </div>
     </form>
 </div>
