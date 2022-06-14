3bd7426ad327a12fb7da1a5be73d92951e32301a
Farez Rahman <farez@redkeydigital.com>
2022-06-14 09:28:40 +0100

    Added Sale model and migration.

---
+++ database/migrations/2022_06_14_080900_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('quantity');
+            $table->float('unit_cost');
+            $table->float('profit_margin');
+            $table->float('shipping_cost');
+            $table->float('selling_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+};
